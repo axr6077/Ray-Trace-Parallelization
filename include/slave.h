f46f2721d884b7e503695b4d4225827ef463e5a2
Ayush Rout <axr6077@rit.edu>
2022-11-22 16:10:56 -0500

    func decl for slave processes

---
+++ include/slave.h
@@ -0,0 +1,9 @@
+#ifndef __SLAVE_PROCESS_H__
+#define __SLAVE_PROCESS_H__
+
+#include "RayTrace.h"
+
+void slaveMain( ConfigData *data );
+void staticCyclesHorizontal(ConfigData *data);
+void slaveDynamicPartition(ConfigData* data);
+#endif
