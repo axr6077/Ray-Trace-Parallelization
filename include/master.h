979cdb57428486c8f98e5d5157e43e92742571fd
Ayush Rout <axr6077@rit.edu>
2022-11-22 16:10:30 -0500

    function decl for master process

---
+++ include/master.h
@@ -0,0 +1,26 @@
+#ifndef __MASTER_PROCESS_H__
+#define __MASTER_PROCESS_H__
+
+#include "RayTrace.h"
+
+//This function is the main that only the master process
+//will run.
+//
+//Inputs:
+//    data - the ConfigData that holds the scene information.
+//
+//Outputs: None
+void masterMain( ConfigData *data );
+
+//This function will perform ray tracing when no MPI use was
+//given.
+//
+//Inputs:
+//    data - the ConfigData that holds the scene information.
+//
+//Outputs: None
+int getIndex(const ConfigData* data, int row, int col);
+void masterSequential(ConfigData *data, float* pixels);
+void staticCyclesHorizontal(ConfigData *data, float* pixels);
+void masterDynamicPartition(ConfigData *data, float* pixels);
+#endif
